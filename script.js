61503b6fb9830806751c61dbb60ca2cb8b989adc
LibelluleRose <0729193@cegepoutaouais.qc.ca>
2023-01-17 21:02:45 -0500

    J'ai modifié les fonctions. l'ajout d'un sac de pomme fonctionne, mais pas les autres et je ne sais pas encore pk. J'ai essayé de faire fonction qui pourrait faire pour tout les fruit, mis en commentaire.

--- script.js
+++ script.js
@@ -1,42 +1,69 @@
 const panier = {
-    pommes : 2,
-    poires : 0,
-    prunes : 0
+    pommes: 2,
+    poires: 2,
+    prunes: 2
 }
 
 
 // Pour afficher le nombre de pommes dans la facture détaillée
 document.querySelector("#qtePommes").textContent = panier.pommes;
 
+// Pour afficher le nombre de poires dans la facture détaillée
+document.querySelector("#qtePoires").textContent = panier.poires;
+
+// Pour afficher le nombre de prunes dans la facture détaillée
+document.querySelector("#qtePrunes").textContent = panier.prunes;
+
 //////////////////////////////////////////////////////////////////////////
 
-
-/*//Initialisation des variables
-let NbrSacPommes = 0;
-let NbrSacPoires = 0;
-let NbrSacPrunes = 0;*/
-
-
-//Quand on click sur le bouton, sa active la fonction qui ajoute le sac à la liste
+//Quand on click sur le bouton, ça active la fonction qui ajoute le sac à la liste
 // et qui met à jour la commande
-function AjouterSacPommes(){
+function AjouterSacPommes() {
 
+//On l'incrémente de 1 la qte dans la const panier.pomme
+    panier.pommes = panier.pommes + 1;
 
-    //On va chercher les valeurs du tableau de facture et on les mets dans des variable
-let qtePommes = document.getElementById("qtePommes").value;
-let poidsPommes = document.getElementById("poidsPommes").value;
-let prixPommes = document.getElementById("prixPommes").value;
+//Pour afficher le nombre de pommes dans la facture détaillée
+   document.querySelector("#qtePommes").textContent = panier.pommes;
 
-//On l'incrémente de 1 chaque nouvelle variable
-    qtePommes = qtePommes + 1;
-    poidsPommes = poidsPommes + 1;
-    prixPommes = prixPommes + 1;
+   //Ajuster le poids total
+
+    //Ajuster le prix total
+
 }
 
-function CalculerTotal
+function AjouterSacPoires() {
+
+//On l'incrémente de 1 la qte dans la const panier.poires
+    panier.pommes = panier.poires + 1;
+
+//Pour afficher le nombre de pommes dans la facture détaillée
+    document.querySelector("#qtePoires").textContent = panier.poires;
+
+    //Ajuster le poids total
+
+    //Ajuster le prix total
+
+}
+
+function AjouterSacPrunes() {
+
+//On l'incrémente de 1 la qte dans la const panier.prunes
+    panier.pommes = panier.prunes + 1;
+
+//Pour afficher le nombre de prunes dans la facture détaillée
+    document.querySelector("#qtePrunes").textContent = panier.poires;
+
+    //Ajuster le poids total
+
+    //Ajuster le prix total
+
+}
 
 
-/*Création des fonctions pour l'achat
-mettre à jour la facture
-lier les fonctions avec les boutons correspondant
- */
+
+/*
+function AjouterSac(fruit){
+    panier.fruit = panier.fruit + 1;
+    document.querySelector(fruit).textContent = panier.fruit;
+}*/
